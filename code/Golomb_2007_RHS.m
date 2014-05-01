a524fabe0537fe3f46f25f2f4aa7a74389fe4f99
benp_mbp <benpolletta@gmail.com>
2014-05-01 15:03:14 -0400

    Working on uniform treatment of dendrites & soma, with vectorized conductances.

---
+++ code/Golomb_2007_RHS.m
@@ -0,0 +1,48 @@
+function [V_out] = Golomb_2007_RHS(t,V_in,no_cells,g_struct,I_app,I_on,noise_multiplier)
+
+    ENa=50;                             %Sodium reversal.
+    sigma_m = 11.5;
+    theta_h = -58.3; sigma_h = -6.7;
+
+    EK=-90;                             %Potassium reversal.
+    theta_n = -12.4; sigma_n = 6.8;     %Delayed rectifier parameters.
+
+    theta_a = -50; sigma_a = 20;        %Potassium (slow inactivation) parameters.
+    theta_b = -70; sigma_b = -6;
+    tau_a = 2; tau_b = 150;
+
+    ERest=-70;                          %Leak reversal.
+
+    Vs_in = V_in(1:no_cells);
+    h_in = V_in(no_cells + (1:no_cells));
+    n_in = V_in(2*no_cells + (1:no_cells));
+    a_in = V_in(3*no_cells + (1:no_cells));
+    b_in = V_in(4*no_cells + (1:no_cells));
+
+    Vs_out = g_struct.gNa.*(steady(Vs_in,g_struct.theta_m,sigma_m).^3).*h_in.*(ENa-Vs_in) + g_struct.gK.*(n_in.^2).*(EK-Vs_in) ...
+      + g_struct.gD.*a_in.^3.*b_in.*(EK-Vs_in) + g_struct.gL.*(ERest-Vs_in) + I_app.*(t > I_on) + noise_multiplier.*randn(no_cells,1); %real(sqrt(2*noise_multiplier*randn));    %Update I-cell voltage.
+    h_out = (steady(Vs_in,theta_h,sigma_h)-h_in)./tau_h(Vs_in);                                            %Update h.
+    n_out = (steady(Vs_in,theta_n,sigma_n)-n_in)./tau_n(Vs_in);                                            %Update n.
+    a_out = (steady(Vs_in,theta_a,sigma_a)-a_in)/tau_a;                                                    %Update a.
+    b_out = (steady(Vs_in,theta_b,sigma_b)-b_in)/tau_b;                                                    %Update b.
+  
+    V_out = [Vs_out; h_out; n_out; a_out; b_out];
+    
+end
+
+%Below, define the auxiliary functions alpha & beta for each gating variable.
+
+function iout = steady(V,theta,sigma)
+iout = 1./(1+exp((theta-V)/sigma));
+end
+
+function th = tau_h(V)
+theta_th = -60; sigma_th = -12;
+th = .5 + 14./(1+exp((theta_th-V)/sigma_th));
+end
+
+function tn = tau_n(V)
+tn_1 = .087 + (11.4)./(1+exp((V+14.6)/8.6));
+tn_2 = .087 + (11.4)./(1+exp((1.3-V)/18.7));
+tn = tn_1.*tn_2;
+end
---
+++ code/Golomb_2007_RK45_w_dendrite.m
@@ -0,0 +1,101 @@
+%Implement a model of a fast-spiking interneuron, as in Golomb 2007.
+
+%INPUTS:
+%  no_cells = number of simulated FSIs.
+%  I0  = Injected current to cell.
+%  tauI= decay time of inhibitory synapse.
+%  T0  = total time of simulation [s].
+%  CS = inhibitory synapse connectivity matrix, including conductance (strength) for each synapse.
+%  CG = gap junction connectivity matrix, including conductance (strength) for each gap junction.
+
+%OUTPUTS:
+%  V = voltage of I-cell.
+%  s = inhibitory synapse.
+%  t = time axis vector (useful for plotting).
+
+function [Vs,Vd,h,n,a,b,s,t] = Golomb_2007_RK45_w_dendrite(no_cells,I_app,T0,in_g_struct,noise_multiplier,gG_multiplier,gS_multiplier)
+
+    % Setting different conductances for soma & dendrites.
+    g_mod_vec = [2*ones(no_cells,1); ones(no_cells,1)/10];
+
+    % Default conductances.
+    g_struct.gNa = 112.5*g_mod_vec;
+    g_struct.gK = 225*g_mod_vec;
+    g_struct.gL = 0.25*g_mod_vec;
+    g_struct.g_sd = 0.25/2*ones(2*no_cells,1);
+    g_struct.theta_m = -24;
+    g_struct.gD = 1.6;
+    g_fields = fieldnames(g_struct);
+
+    % Assigning conductances from in_g_struct.
+    in_fields = fieldnames(in_g_struct);
+    for field = in_fields'
+        if ~any(strcmpi(char(field), g_fields))
+            error(['Bad option field name: ',char(field)]);
+        end
+        if ~isempty(eval(['in_g_struct.',char(field)])) % empty values are discarded
+            eval(['g_struct.', char(field), ' = in_g_struct.', char(field),';']);
+        end
+    end
+
+
+    dt = 0.005;                       %The time step.
+    T  = ceil(T0/dt);
+    t = (1:T)*dt;                     %Define time axis vector (useful for plotting).
+
+    %% Initial conditions.
+    
+    Vs_0 = -70+7*rand(no_cells,1);  	%Set the initial conditions for P-cells, I-cells, and synapses.
+    Vd_0 = Vs_0;
+    h_0 = 0.0 + 0.1*rand(2*no_cells,1);
+    n_0 = 0.0 + 0.1*rand(2*no_cells,1);
+    a_0 = 0.0 + 0.1*rand(2*no_cells,1);
+    b_0 = 0.0 + 0.1*rand(2*no_cells,1);
+    s_0 = 0.0 + 0.1*rand(no_cells,1);
+    I_on = 100;%7*rand(no_cells,1);
+
+    V0 = [Vs_0; Vd_0; h_0; n_0; a_0; b_0; s_0];
+    
+    %% Connectivity matrices.
+    
+    [CS,CG] = striatal_connectivity_matrices(no_cells,0,1);
+    
+    if nargin >= 7    % Doubling gap junction conductances, unless otherwise specified.
+        
+        if ~isempty(gG_multiplier)
+            
+            CG = gG_multiplier*CG;
+            
+        else
+            
+            CG = 2*CG;
+            
+        end
+        
+    elseif nargin < 7
+        
+        CG = 2*CG;
+        
+    end
+    
+    if nargin > 7 && ~isempty(gS_multiplier)
+        
+        CS = gS_multiplier*CS;
+        
+    end
+
+    CG = [zeros(no_cells) diag(g_struct.g_sd); diag(g_struct.g_sd) CG];
+    CS = [CS zeros(no_cells); zeros(no_cells,2*no_cells)];
+    
+    %% Perform integration using Runge-Kutta 4/5.
+    [t, V] = ode45(@(t, V) Golomb_2007_RHS(t,V,no_cells,CS,CG,g_struct,I_app,I_on,noise_multiplier),t,V0);%dt*[1 T],V0);
+    
+    Vs = V(:,1:no_cells)';
+    Vd = V(:,no_cells + (1:no_cells))';
+    h = V(:,2*no_cells + (1:2*no_cells))';
+    n = V(:,4*no_cells + (1:2*no_cells))';
+    a = V(:,6*no_cells + (1:2*no_cells))';
+    b = V(:,8*no_cells + (1:2*no_cells))';
+    s = V(:,10*no_cells + (1:no_cells))';
+  
+end
