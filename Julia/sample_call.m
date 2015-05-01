209e77e1eb9e85f6676408414a8494215a795de9
benp <benpolletta@gmail.com>
2015-05-01 16:29:12 -0400

    Renaming.

--- Julia/sample_call.m
+++
@@ -1,9 +0,0 @@
-input = rand(10, ceil(100/.005));
-
-input = input < 1000*.005/1000;
-
-for i = 1:10, input(i, :) = conv(double(input(i,:)), ones(1,200), 'same'); end
-
-[Vs,Vd,s,m,h,n,t] = ing_w_dendritic_gap_jxn(10, input*70, 100, [], zeros(10), .3*(rand(10) > .4));
-
-plot(Vs')
--- code/ode45_rhs_test.m
+++
@@ -1,3 +0,0 @@
-function x_out = ode45_rhs_test(t,x_in,n)
-
-x_out = n*x_in;
