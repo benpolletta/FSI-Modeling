7b7f5141cb54b154381b768ed4b0dbc8f9a534af
benp <benpolletta@gmail.com>
2016-07-29 17:50:46 -0400

    Changing Gutfreund_KCa to use pulses; changing Gutfreund_plots & gutfreund_metrics to work with variables besides voltage; changing iKCaT to take a single value (not times 10^(-5)) to multiply cai.

--- Theta_RS/Gutfreund_plots.m
+++ Theta_RS/Gutfreund_plots.m
@@ -1,122 +1,138 @@
-function results = Gutfreund_plots(data, results)
+function results = Gutfreund_plots(data, results, variables)
 
-varied = data(1).simulator_options.vary;
-
-no_varied = size(varied, 1);
-
-[vary_labels, vary_vectors] = deal(cell(no_varied));
-
-for v = 1:no_varied
-    
-    vary_labels{v} = varied{v, 2};
-    
-    vary_vectors{v} = varied{v, 3};
-    
-    vary_lengths(v) = length(varied{v, 3});
-    
-end
-    
-rfp_lengths = [vary_lengths([1 2]) prod(vary_lengths(3:end))];
-
-if no_varied > 2
-    
-    no_figures = rfp_lengths(end);
-    
-    vary_lengths_cp = cumprod(vary_lengths(3:end));
-    
-    figure_params = nan(no_figures, no_varied - 2);
-    
-    for p = 1:no_figures
-        
-        figure_params(p, 1) = vary_vectors{3}(mod(p - 1, vary_lengths(3)) + 1);
-        
-        for v = 4:no_varied
-            
-            figure_params(p, v - 2) = vary_vectors{v}(ceil(p/vary_lengths_cp(v - 3)));
-            
-        end
-        
-    end
-        
-else
-
-    no_figures = 1;
-    
-end
+if ~iscell(variables), variables = {variables}; end
 
 if isempty(results)
     
-    results = AnalyzeStudy(data, @gutfreund_metrics);
+    for var = 1:length(variables)
+        
+        results{var} = AnalyzeStudy(data, @gutfreund_metrics, 'variable', variables{var});
+        
+    end
     
 end
 
-result_fields = fieldnames(results);
-
-[r, c] = subplot_size(length(result_fields));
-
-nz_amp = nan(rfp_lengths(2), rfp_lengths(1));
-
-for f = length(result_fields):-1:1
+for var = 1:length(variables)
     
-    eval(sprintf('results_for_plot = [results(:).%s];', result_fields{f}))
+    varied = data(1).simulator_options.vary;
     
-    results_for_plot = reshape(results_for_plot, fliplr(rfp_lengths));
+    no_varied = size(varied, 1);
     
-    for p = 1:no_figures
+    [vary_labels, vary_vectors] = deal(cell(no_varied));
+    
+    for variable = 1:no_varied
         
-        rfp_p = reshape(results_for_plot(p, :, :), rfp_lengths([2 1]));
+        vary_labels{variable} = varied{variable, 2};
         
-        if f == length(result_fields)
+        vary_vectors{variable} = varied{variable, 3};
+        
+        vary_lengths(variable) = length(varied{variable, 3});
+        
+    end
+    
+    rfp_lengths = [vary_lengths([1 2]) prod(vary_lengths(3:end))];
+    
+    if no_varied > 2
+        
+        no_figures = rfp_lengths(end);
+        
+        vary_lengths_cp = cumprod(vary_lengths(3:end));
+        
+        figure_params = nan(no_figures, no_varied - 2);
+        
+        for p = 1:no_figures
             
-            nz_amp(:, :, p) = rfp_p > .1;
+            figure_params(p, 1) = vary_vectors{3}(mod(p - 1, vary_lengths(3)) + 1);
+            
+            for v = 4:no_varied
+                
+                figure_params(p, v - 2) = vary_vectors{v}(ceil(p/vary_lengths_cp(v - 3)));
+                
+            end
             
         end
         
-        rfp_p(nz_amp(:, :, p) == 0) = nan;
+    else
         
-        figure(p)
-
-        subplot(r, c, f)
-        
-        imagesc(vary_vectors{1}, vary_vectors{2}, rfp_p)
-        
-        colorbar
-        
-        xlabel(vary_labels{1}, 'Interpreter', 'none')
-        
-        ylabel(vary_labels{2}, 'Interpreter', 'none')
-        
-        title(result_fields{f}, 'Interpreter', 'none')
-        
-    end
-
-end
-
-for p = 1:no_figures
-    
-    vary_title = '';
-    
-    vary_name = '';
-    
-    for v = 1:2
-        
-        vary_name = [vary_name, sprintf('_%s_%fto%f', vary_labels{v}, vary_vectors{v}([1 end]))];
+        no_figures = 1;
         
     end
     
-    for v = 1:(no_varied - 2)
+    for p = 1:no_figures
         
-        vary_title = [vary_title, sprintf('%s = %f ', vary_labels{v + 2}, figure_params(p, v))];
-        
-        vary_name = [vary_name, sprintf('_%s%0.2e', vary_labels{v + 2}, figure_params(p, v))];
+        handle(p) = figure;
         
     end
     
-    figure(p)
+    result = results{var};
     
-    mtit(p, vary_title, 'FontSize', 14, 'yoff', .2)
+    result_fields = fieldnames(result);
     
-    save_as_pdf(p, ['gutfreund', vary_name])
+    [r, c] = subplot_size(length(result_fields));
+    
+    % nz_amp = nan(rfp_lengths(2), rfp_lengths(1));
+    
+    for f = length(result_fields):-1:1
+        
+        eval(sprintf('results_for_plot = [result(:).%s];', result_fields{f}))
+        
+        results_for_plot = reshape(results_for_plot, fliplr(rfp_lengths));
+        
+        for p = 1:no_figures
+            
+            rfp_p = reshape(results_for_plot(p, :, :), rfp_lengths([2 1]));
+            
+            % if f == length(result_fields)
+            % 
+            %     nz_amp(:, :, p) = rfp_p > .1;
+            % 
+            % end
+            %
+            % rfp_p(nz_amp(:, :, p) == 0) = nan;
+            
+            figure(handle(p))
+            
+            subplot(r, c, f)
+            
+            imagesc(vary_vectors{1}, vary_vectors{2}, rfp_p)
+            
+            colorbar
+            
+            xlabel(vary_labels{1}, 'Interpreter', 'none')
+            
+            ylabel(vary_labels{2}, 'Interpreter', 'none')
+            
+            title(result_fields{f}, 'Interpreter', 'none')
+            
+        end
+        
+    end
+    
+    for p = 1:no_figures
+        
+        vary_title = '';
+        
+        vary_name = '';
+        
+        for variable = 1:2
+            
+            vary_name = [vary_name, sprintf('_%s_%gto%g', vary_labels{variable}, vary_vectors{variable}([1 end]))];
+            
+        end
+        
+        for variable = 1:(no_varied - 2)
+            
+            vary_title = [vary_title, sprintf('%s = %g ', vary_labels{variable + 2}, figure_params(p, variable))];
+            
+            vary_name = [vary_name, sprintf('_%s%g', vary_labels{variable + 2}, figure_params(p, variable))];
+            
+        end
+        
+        mtit(handle(p), vary_title, 'FontSize', 14, 'yoff', .2)
+        
+        save_as_pdf(handle(p), ['Figures/gutfreund_metrics', vary_name, '_', variables{var}])
+        
+    end
     
 end
 
--- Theta_RS/gutfreund_metrics.m
+++ Theta_RS/gutfreund_metrics.m
@@ -1,12 +1,24 @@
-function results = gutfreund_metrics(data)
+function results = gutfreund_metrics(data, varargin)
 
-v = data.pop1_v;
+if ~isempty(varargin)
+    
+    if strcmp(varargin{1}, 'variable')
+        
+        variable = varargin{2};
+        
+    end
+    
+end
+
+v = getfield(data, variable);
 
 t = data.time;
 
 rmp = mean(v(t >= 100 & t <= 500));
 
-v_injected = v(t >= 1501 & t <= 3500);
+t_lims = [3001 6000];
+
+v_injected = v(t >= t_lims(1) & t <= t_lims(2));
 
 [v_hat, f] = pmtm(detrend(v_injected), 2, [], 1000*length(t)/t(end), 'eigen');
 
@@ -18,13 +30,13 @@ v_hat_smoothed = conv(v_hat, gauss_kernel, 'same');
 
 % v_hat_smoothed = v_hat;
 
-max_freq = f(find(v_hat_smoothed == max(v_hat_smoothed)));
+max_freq = f(v_hat_smoothed == max(v_hat_smoothed));
 
-sto_index = t >= 501 & t <= 1500;
+% sto_index = t >= 1001 & t <= 5000;
 
-low = min(v_injected(sto_index)); high = max(v_injected(sto_index));
+low = min(v_injected); high = max(v_injected); % low = min(v_injected(sto_index)); high = max(v_injected(sto_index));
 
-rmp_sto = mean(v_injected(sto_index));
+rmp_sto = mean(v_injected); % mean(v_injected(sto_index));
 
 amp = high - low;
 
