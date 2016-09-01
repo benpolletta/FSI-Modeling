30de27464357afcd19539139ff3403bd11bf6d04
benpolletta <benpolletta@gmail.com>
2016-08-31 20:42:51 -0400

    Scripts for getting phase information from simulations.

---
+++ Theta_RS/phase_metrics.m
@@ -0,0 +1,93 @@
+function results = phase_metrics(data, varargin)
+
+if ~isempty(varargin)
+    
+    if strcmp(varargin{1}, 'variable')
+        
+        variable = varargin{2};
+        
+    end
+    
+else
+    
+    variable = 'pop1_v';
+    
+end
+
+v = getfield(data, variable);
+
+t = data.time;
+
+sampling_freq = 1000*length(t)/t(end);
+
+%% Getting peak frequency.
+
+[v_hat, f] = pmtm(detrend(v), 2, [], sampling_freq, 'eigen');
+
+gauss_kernel = normpdf(-2:.2:2, 0, .5);
+
+gauss_kernel = gauss_kernel/sum(gauss_kernel);
+
+v_hat_smoothed = conv(v_hat, gauss_kernel, 'same');
+
+max_freq = f(v_hat_smoothed == max(v_hat_smoothed));
+
+freqs = [1.5 4.5 max_freq]; no_cycles = [3 3 3];
+
+%% Getting wavelet components.
+
+v_bandpassed = wavelet_spectrogram(v, sampling_freq, freqs, no_cycles, 0, '');
+
+v_phase = angle(wavelet_spectrogram);
+
+%% Getting spike times and computing spike phases.
+
+v_spikes = [diff(v > 0) == 1; zeros(size(v, 2))];
+
+v_spike_phases = v_phase(logical(v_spikes));
+
+%% Computing phase-phase relationships.
+
+no_bins = 18;
+
+bin_centers = 1:no_bins;
+
+bin_centers = (bin_centers - 1)*2*pi/no_bins - pi*(nobins - 1)/no_bins;
+
+bin_left_endpoints = bin_centers - pi/no_bins;
+
+bin_right_endpoints = bin_centers + pi/no_bins;
+
+f_pairs = nchoosek(1:no_freqs, 2); no_f_pairs = size(f_pairs, 1);
+
+[v_phase_coh, v_phase_angle] = deal(nan(no_bins, no_freqs, no_freqs));
+
+v_phase_phase = nan(no_bins, no_bins, no_f_pairs);
+
+for fp = 1:no_f_pairs
+    
+    for b1 = 1:no_bins
+        
+        b1_indicator = v_phase(:, f_pairs(fp, 1)) >= bin_left_endpoints(b1) & v_phase(:, f_pairs(fp, 1)) < bin_right_endpoints(b1);
+        
+        v_phase_coh(b1, f_pairs(fp, 2), f_pairs(fp, 1)) = circ_r(v_phase(b1_indicator, f_pairs(fp, 2)));
+        
+        v_phase_angle(b1, f_pairs(fp, 2), f_pairs(fp, 1)) = circ_mean(v_phase(b1_indicator, f_pairs(fp, 2)));
+        
+        for b2 = 1:no_bins
+   
+            b2_indicator = v_phase(:, f_pairs(fp, 2)) >= bin_left_endpoints(b2) & v_phase(:, f_pairs(fp, 2)) < bin_right_endpoints(b2);
+        
+            v_phase_coh(b2, f_pairs(fp, 1), f_pairs(fp, 2)) = circ_r(v_phase(b2_indicator, f_pairs(fp, 1)));
+        
+            v_phase_angle(b2, f_pairs(fp, 1), f_pairs(fp, 2)) = circ_mean(v_phase(b2_indicator, f_pairs(fp, 1)));
+            
+            v_phase_phase(b2, b1, fp) = sum(b1_indicator & b2_indicator);
+    
+        end
+        
+    end
+    
+end
+
+results = struct('v_spike_phases', v_spike_phases, 'v_phase_coh', v_phase_coh, 'v_phase_angle', v_phase_angle, 'v_phase_phase', v_phase_phase);
---
+++ Theta_RS/phase_plots.m
@@ -0,0 +1,140 @@
+function results = phase_plots(data, results, variables)
+
+if ~iscell(variables), variables = {variables}; end
+
+if isempty(results)
+    
+    for var = 1:length(variables)
+        
+        results{var} = AnalyzeStudy(data, @phase_metrics, 'variable', variables{var});
+        
+    end
+    
+end
+
+for var = 1:length(variables)
+    
+    varied = data(1).simulator_options.vary;
+    
+    no_varied = size(varied, 1);
+    
+    [vary_labels, vary_vectors] = deal(cell(no_varied));
+    
+    for variable = 1:no_varied
+        
+        vary_labels{variable} = varied{variable, 2};
+        
+        vary_vectors{variable} = varied{variable, 3};
+        
+        vary_lengths(variable) = length(varied{variable, 3});
+        
+    end
+    
+    rfp_lengths = [vary_lengths([1 2]) prod(vary_lengths(3:end))];
+    
+    if no_varied > 2
+        
+        no_figures = rfp_lengths(end);
+        
+        vary_lengths_cp = cumprod(vary_lengths(3:end));
+        
+        figure_params = nan(no_figures, no_varied - 2);
+        
+        for p = 1:no_figures
+            
+            figure_params(p, 1) = vary_vectors{3}(mod(p - 1, vary_lengths(3)) + 1);
+            
+            for v = 4:no_varied
+                
+                figure_params(p, v - 2) = vary_vectors{v}(ceil(p/vary_lengths_cp(v - 3)));
+                
+            end
+            
+        end
+        
+    else
+        
+        no_figures = 1;
+        
+    end
+    
+    for p = 1:no_figures
+        
+        handle(p) = figure;
+        
+    end
+    
+    result = results{var};
+    
+    result_fields = fieldnames(result);
+    
+    [r, c] = subplot_size(length(result_fields));
+    
+    % nz_amp = nan(rfp_lengths(2), rfp_lengths(1));
+    
+    for f = length(result_fields):-1:1
+        
+        eval(sprintf('results_for_plot = [result(:).%s];', result_fields{f}))
+        
+        results_for_plot = reshape(results_for_plot, fliplr(rfp_lengths));
+        
+        for p = 1:no_figures
+            
+            rfp_p = reshape(results_for_plot(p, :, :), rfp_lengths([2 1]));
+            
+            % if f == length(result_fields)
+            % 
+            %     nz_amp(:, :, p) = rfp_p > .1;
+            % 
+            % end
+            %
+            % rfp_p(nz_amp(:, :, p) == 0) = nan;
+            
+            figure(handle(p))
+            
+            subplot(r, c, f)
+            
+            imagesc(vary_vectors{1}, vary_vectors{2}, rfp_p)
+            
+            colorbar
+            
+            xlabel(vary_labels{1}, 'Interpreter', 'none')
+            
+            ylabel(vary_labels{2}, 'Interpreter', 'none')
+            
+            title(result_fields{f}, 'Interpreter', 'none')
+            
+        end
+        
+    end
+    
+    for p = 1:no_figures
+        
+        figure(handle(p))
+        
+        vary_title = '';
+        
+        vary_name = '';
+        
+        for variable = 1:2
+            
+            vary_name = [vary_name, sprintf('_%s_%gto%g', vary_labels{variable}, vary_vectors{variable}([1 end]))];
+            
+        end
+        
+        for variable = 1:(no_varied - 2)
+            
+            vary_title = [vary_title, sprintf('%s = %g ', vary_labels{variable + 2}, figure_params(p, variable))];
+            
+            vary_name = [vary_name, sprintf('_%s%g', vary_labels{variable + 2}, figure_params(p, variable))];
+            
+        end
+        
+        mtit(handle(p), vary_title, 'FontSize', 14, 'yoff', .2)
+        
+        save_as_pdf(handle(p), ['Figures/gutfreund_metrics', vary_name, '_', variables{var}])
+        
+    end
+    
+end
+
