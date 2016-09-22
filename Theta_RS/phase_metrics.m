da2d292f07393304151a54c161f7de956ab3c7f5
benp <benpolletta@gmail.com>
2016-09-22 16:21:03 -0400

    Adding pulses to Gutfreund_original; adding floor to time constant in iKCaT.mech; removing code for aperiodic pulses from iPeriodicPulses.mech; changing plotting, what's saved, lots of stuff in phase_metrics.

--- Theta_RS/phase_metrics.m
+++ Theta_RS/phase_metrics.m
@@ -1,18 +1,26 @@
-function results = phase_metrics(data, varargin, figure_flag)
+function results = phase_metrics(data, varargin)
 
 if ~isempty(varargin)
     
-    if strcmp(varargin{1}, 'variable')
+    for v = 1:(length(varargin)/2)
         
-        variable = varargin{2};
+        if strcmp(varargin{2*v - 1}, 'variable')
+            
+            variable = varargin{2*v};
+            
+        elseif strcmp(varargin{2*v - 1}, 'figure_flag')
+            
+            figure_flag = varargin{2*v};
+            
+        end
         
     end
     
-else
-    
-    variable = 'pop1_v';
-    
 end
+
+if ~exist('variable', 'var'), variable = 'pop1_v'; end
+
+if ~exist('figure_flag', 'var'), figure_flag = 0; end
 
 v = getfield(data, variable);
 
@@ -32,9 +40,9 @@ gauss_kernel = gauss_kernel/sum(gauss_kernel);
 
 v_hat_smoothed = conv(v_hat, gauss_kernel, 'same');
 
-max_freq = f(v_hat_smoothed == max(v_hat_smoothed));
+peak_freq = f(v_hat_smoothed == max(v_hat_smoothed));
 
-freqs = [1.5 4.5 max_freq]'; no_cycles = [7 7 7]'; no_freqs = length(freqs);
+freqs = [1.5 4.5 peak_freq]'; no_cycles = [7 7 7]'; no_freqs = length(freqs);
 
 %% Getting wavelet components.
 
@@ -46,19 +54,33 @@ v_phase = angle(v_bandpassed);
 
 if figure_flag
 
-    figure, subplot(4, 1, 1), plot(f(f <= 15), v_hat(f <= 15))
+    figure, subplot(4, 1, 1)
     
-    t_end = t >= 0; % 3000;
+    t_cutoff = 6000;
     
-    for frequency = 1:3, 
+    [v_hat_fig, f_fig] = pmtm(detrend(v(t <= 6000)), 2, [], sampling_freq, 'eigen');
+    
+    plot(f_fig(f_fig <= 15), v_hat_fig(f_fig <= 15))
+    
+    t_end = t(t <= 6000)/1000; % 3000;
+    
+    for frequency = 1:3
         
         subplot(4, 2, 2 + 2*(frequency - 1) + 1)
         
-        plotyy(t(t_end), real(v_bandpassed(t_end, frequency)), t(t_end), v(t_end))
+        ax = plotyy(t_end, real(v_bandpassed(t <= 6000, frequency)), t_end, v(t <= 6000));
+        
+        ylabel([num2str(freqs(frequency), '%.2g'), ' Oscillation'], 'FontSize', 12)
+        
+        for a = 1:2, set(ax(a), 'XLim', [t_end(1) t_end(end)]), end
     
         subplot(4, 2, 2 + 2*(frequency - 1) + 2)
         
-        plotyy(t(t_end), angle(v_bandpassed(t_end, frequency)), t(t_end), v(t_end))
+        ax = plotyy(t_end, angle(v_bandpassed(t <= 6000, frequency)), t_end, v(t <= 6000));
+        
+        ylabel([num2str(freqs(frequency), '%.2g'), ' Phase'], 'FontSize', 12)
+        
+        for a = 1:2, set(ax(a), 'XLim', [t_end(1) t_end(end)]), end
         
     end
     
@@ -66,70 +88,98 @@ end
 
 %% Getting spike times and computing spike phases.
 
-v_spikes = [diff(v > 0) == 1; zeros(size(v, 2))];
+v_spikes = [diff(v > 0) == 1; zeros(1, size(v, 2))];
 
-v_spike_phases = v_phase(repmat(logical(v_spikes), 1, no_freqs));
+no_periods = 2; pd_length = t(end)/no_periods; t_pd = nan(length(t), no_periods);
 
-if figure_flag
+no_spikes = nan(no_periods, 1);
+
+for p = 1:no_periods
    
-    figure
-    
-    subplot(2, 1, 1)
-    
-    plotyy(t, v, t, v_spikes)
-    
-    for i = 1:3
-        
-        subplot(2, 3, 3 + i)
-        
-        rose(gca, v_spike_phases(:, i))
-    
-    end
+   t_pd(:, p) = t > (p - 1)*pd_length & t <= p*pd_length;
+   
+   no_spikes(p) = sum(v_spikes & t_pd(:, p));
     
 end
 
-%% Computing phase-phase relationships.
+v_spike_phases = nan(max(no_spikes), no_periods, no_freqs);
 
-no_bins = 18;
+% colors = {[1 1 0]; [1 0 1]; [0 1 1]};
 
-bin_centers = 1:no_bins;
-
-bin_centers = (bin_centers - 1)*2*pi/no_bins - pi*(no_bins - 1)/no_bins;
-
-bin_left_endpoints = bin_centers - pi/no_bins;
-
-bin_right_endpoints = bin_centers + pi/no_bins;
-
-f_pairs = nchoosek(1:no_freqs, 2); no_f_pairs = size(f_pairs, 1);
-
-[v_phase_coh, v_phase_angle] = deal(nan(no_bins, no_freqs, no_freqs));
-
-v_phase_phase = nan(no_bins, no_bins, no_f_pairs);
-
-for fp = 1:no_f_pairs
+% if figure_flag
+%    
+%     figure
+%     
+%     subplot(no_periods + 1, 1, 1)
+%     
+%     plotyy(t, v, t, v_spikes)
     
-    for b1 = 1:no_bins
-        
-        b1_indicator = v_phase(:, f_pairs(fp, 1)) >= bin_left_endpoints(b1) & v_phase(:, f_pairs(fp, 1)) < bin_right_endpoints(b1);
-        
-        v_phase_coh(b1, f_pairs(fp, 2), f_pairs(fp, 1)) = circ_r(v_phase(b1_indicator, f_pairs(fp, 2)));
-        
-        v_phase_angle(b1, f_pairs(fp, 2), f_pairs(fp, 1)) = circ_mean(v_phase(b1_indicator, f_pairs(fp, 2)));
-        
-        for b2 = 1:no_bins
-   
-            b2_indicator = v_phase(:, f_pairs(fp, 2)) >= bin_left_endpoints(b2) & v_phase(:, f_pairs(fp, 2)) < bin_right_endpoints(b2);
-        
-            v_phase_coh(b2, f_pairs(fp, 1), f_pairs(fp, 2)) = circ_r(v_phase(b2_indicator, f_pairs(fp, 1)));
-        
-            v_phase_angle(b2, f_pairs(fp, 1), f_pairs(fp, 2)) = circ_mean(v_phase(b2_indicator, f_pairs(fp, 1)));
-            
-            v_phase_phase(b2, b1, fp) = sum(b1_indicator & b2_indicator);
+for f = 1:no_freqs
     
-        end
+    for p = 1:no_periods
+        
+        % subplot(no_periods + 1, no_freqs, p*no_freqs + f)
+        
+        v_spike_phases(1:no_spikes(p), p, f) = v_phase(logical(v_spikes & t_pd(:, p)), f);
+        
+        % h = rose(gca, v_spike_phases(:, p, f));
+        %
+        % set(h, 'LineWidth', 2, 'Color', colors{p})
+        %
+        % % x = get(h, 'XData'); y = get(h, 'YData');
+        % %
+        % % patch(x, y, colors{p})
+        %
+        % hold on
         
     end
     
 end
+    
+% end
 
-results = struct('v_spike_phases', v_spike_phases, 'v_phase_coh', v_phase_coh, 'v_phase_angle', v_phase_angle, 'v_phase_phase', v_phase_phase);
+% %% Computing phase-phase relationships.
+% 
+% no_bins = 18;
+% 
+% bin_centers = 1:no_bins;
+% 
+% bin_centers = (bin_centers - 1)*2*pi/no_bins - pi*(no_bins - 1)/no_bins;
+% 
+% bin_left_endpoints = bin_centers - pi/no_bins;
+% 
+% bin_right_endpoints = bin_centers + pi/no_bins;
+% 
+% f_pairs = nchoosek(1:no_freqs, 2); no_f_pairs = size(f_pairs, 1);
+% 
+% [v_phase_coh, v_phase_angle] = deal(nan(no_bins, no_freqs, no_freqs));
+% 
+% v_phase_phase = nan(no_bins, no_bins, no_f_pairs);
+% 
+% for fp = 1:no_f_pairs
+%     
+%     for b1 = 1:no_bins
+%         
+%         b1_indicator = v_phase(:, f_pairs(fp, 1)) >= bin_left_endpoints(b1) & v_phase(:, f_pairs(fp, 1)) < bin_right_endpoints(b1);
+%         
+%         v_phase_coh(b1, f_pairs(fp, 2), f_pairs(fp, 1)) = circ_r(v_phase(b1_indicator, f_pairs(fp, 2)));
+%         
+%         v_phase_angle(b1, f_pairs(fp, 2), f_pairs(fp, 1)) = circ_mean(v_phase(b1_indicator, f_pairs(fp, 2)));
+%         
+%         for b2 = 1:no_bins
+%    
+%             b2_indicator = v_phase(:, f_pairs(fp, 2)) >= bin_left_endpoints(b2) & v_phase(:, f_pairs(fp, 2)) < bin_right_endpoints(b2);
+%         
+%             v_phase_coh(b2, f_pairs(fp, 1), f_pairs(fp, 2)) = circ_r(v_phase(b2_indicator, f_pairs(fp, 1)));
+%         
+%             v_phase_angle(b2, f_pairs(fp, 1), f_pairs(fp, 2)) = circ_mean(v_phase(b2_indicator, f_pairs(fp, 1)));
+%             
+%             v_phase_phase(b2, b1, fp) = sum(b1_indicator & b2_indicator);
+%     
+%         end
+%         
+%     end
+%     
+% end
+
+results = struct('v_spike_phases', v_spike_phases, 'peak_freq', peak_freq); % , 'v_phase_coh', v_phase_coh, 'v_phase_angle', v_phase_angle, 'v_phase_phase', v_phase_phase);
