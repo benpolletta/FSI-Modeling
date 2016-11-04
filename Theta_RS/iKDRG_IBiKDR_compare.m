6b5628672511dc3ae6fbd8339eb41e6432006e09
benpolletta <benpolletta@gmail.com>
2016-11-04 17:21:56 -0400

    Added offset to IBiKDR.mech, and fast_offset variable to equations in Gutfreund_Dave_spiking; changed default offset in IBiNaF.mech; added titles and legends in *compare.

--- Theta_RS/iKDRG_IBiKDR_compare.m
+++ Theta_RS/iKDRG_IBiKDR_compare.m
@@ -10,9 +10,24 @@ figure
 
 subplot(211)
 
-plotyy(v', [iKDRG_minf.^4; IBiKDR_minf.^4]', v', [iKDRG_mtau; IBiKDR_mtau]')
+[~, h1, h2] = plotyy(v', [iKDRG_minf.^4; IBiKDR_minf.^4]', v', [iKDRG_mtau; IBiKDR_mtau]');
+
+title('Potassium Activation Curves', 'FontSize', 16)
+
+legend(h1, {'Gutfreund''s m_{\infty}^4', 'Dave''s m_{\infty}^4'}, 'Location', 'E', 'FontSize', 12)
+
+legend(h2, {'Gutfreund''s \tau_m', 'Dave''s \tau_m'}, 'Location', 'W', 'FontSize', 12)
 
 subplot(212)
 
-plotyy(v', [iKDRG_minf.^4 - IBiKDR_minf.^4]', v', [iKDRG_mtau - IBiKDR_mtau]')
+[~, h1, h2] = plotyy(v', [iKDRG_minf.^4 - IBiKDR_minf.^4]', v', [iKDRG_mtau - IBiKDR_mtau]');
+
+legend(h1, {'Gutfreund''s m_{\infty}^4 - Dave''s m_{\infty}^4'}, 'Location', 'SE', 'FontSize', 12)
+
+legend(h2, {'Gutfreund''s \tau_m - Dave''s \tau_m'}, 'Location', 'W', 'FontSize', 12)
+
+xlabel('Voltage (mV)', 'FontSize', 14)
+
+
+save_as_pdf(gcf, 'iKDRG_IBiKDR_compare')
 
--- Theta_RS/iNaG_IBiNaF_compare.m
+++ Theta_RS/iNaG_IBiNaF_compare.m
@@ -10,13 +10,32 @@ figure
 
 subplot(311)
 
-plotyy(v', [iNaG_m; iNaF_m]', v', [iNaG_h; iNaF_h]')
+[~, h1, h2] = plotyy(v', [iNaG_m; iNaF_m]', v', [iNaG_h; iNaF_h]');
+
+title('Sodium Activation Curves', 'FontSize', 16)
+
+legend(h1, {'Gutfreund''s m_{\infty}', 'Dave''s m_{\infty}'}, 'Location', 'E', 'FontSize', 12)
+
+legend(h2, {'Gutfreund''s h_{\infty}', 'Dave''s h_{\infty}'}, 'Location', 'W', 'FontSize', 12)
 
 subplot(312)
 
-plotyy(v', [(iNaG_m.^3).*iNaG_h; (iNaF_m.^3).*iNaF_h]', v', [iNaG_ht; iNaF_ht]')
+[~, h1, h2] = plotyy(v', [(iNaG_m.^3).*iNaG_h; (iNaF_m.^3).*iNaF_h]', v', [iNaG_ht; iNaF_ht]');
+
+legend(h1, {'Gutfreund''s m^3h', 'Dave''s m^3h'}, 'Location', 'E', 'FontSize', 12)
+
+legend(h2, {'Gutfreund''s \tau_h', 'Dave''s \tau_h'}, 'Location', 'W', 'FontSize', 12)
 
 subplot(313)
 
-plotyy(v', [(iNaG_m.^3).*iNaG_h - (iNaF_m.^3).*iNaF_h]', v', [iNaG_ht - iNaF_ht]')
+[~, h1, h2] = plotyy(v', [(iNaG_m.^3).*iNaG_h - (iNaF_m.^3).*iNaF_h]', v', [iNaG_ht - iNaF_ht]');
+
+legend(h1, {'Gutfreund''s m^3h - Dave''s m^3h'}, 'Location', 'NW', 'FontSize', 12)
+
+legend(h2, {'Gutfreund''s \tau_h - Dave''s \tau_h'}, 'Location', 'SE', 'FontSize', 12)
+
+xlabel('Voltage (mV)', 'FontSize', 14)
+
+
+save_as_pdf(gcf, 'iNaG_IBiNaF_compare')
 
