51d7db799df6ee9d1785ef3289e18591e93ac9d8
benpolletta <benpolletta@gmail.com>
2018-01-17 17:14:46 -0500

    Gutfreund_KCa_PRC: changing STPonset value.

--- Theta_RS/Gutfreund_KCa_PRC.m
+++ Theta_RS/Gutfreund_KCa_PRC.m
@@ -1,4 +1,6 @@
 
-  [data, name] = Gutfreund_KCa_PRC_model(0, 6000, 0, 'I_app', 0.87, 'STPstim', 0:-0.1:-0.5, 'STPwidth', 30, 'STPshift', 0:15:600, 'STPkernelType', 25, 'STPonset', 1000);
+  [data, name] = Gutfreund_KCa_PRC_model(0, 6000, 0, 'I_app', 0.87,...
+      'STPstim', 0:-0.1:-0.5, 'STPwidth', 30, 'STPshift', 0:15:600,...
+      'STPkernelType', 25, 'STPonset', 500);
   
   results = Gutfreund_KCa_PRC_plot(data, [], name);
